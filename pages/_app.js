a131122f0a4932251f8d0c60995b172d0a034757
Marian Ely Villa <maryanvilla489@gmail.com>
2025-09-20 12:06:59 +0800

    prelim

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css'
+import layout from './layout'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <layout>
+      <Component {...pageProps} />
+    </layout>
+  )
 }
